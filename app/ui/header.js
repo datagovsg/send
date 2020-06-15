1c3c4e44422cd334eb6b1b3b4f83059f886ce8e8
DigiPie <evan@open.gov.sg>
2020-06-15 12:05:25 +0800

    refactor: header.js

--- app/ui/header.js
+++ app/ui/header.js
@@ -18,41 +18,28 @@ class Header extends Component {
   }
 
   createElement() {
-    const title =
-      platform() === 'android'
-        ? html`
-            <a class="flex flex-row items-center">
-              <img src="${assets.get('icon.svg')}" />
-            </a>
-          `
-        : html`
-            <div class="flex">
-              <a
-                class="flex flex-row items-center"
-                href="${window.DEFAULTS.LOGIN_URL}"
-              >
-                <img
-                  alt="${this.state.translate('title')}"
-                  src="${assets.get('icon.svg')}"
-                />
-              </a>
-            </div>
-            <div class="main-nav flex">
-              <a href="${window.DEFAULTS.LOGIN_URL}">
-                <i class="bx bx-arrow-back"></i>
-                <div>Return to Vault</div>
-              </a>
-              <a href="/vault-logout">
-                <i class="bx bx-exit"></i>
-                <div>Logout</div>
-              </a>
-            </div>
-          `;
     return html`
       <header
         class="main-header relative flex-none flex flex-row w-full z-20 bg-white"
       >
-        ${title} ${this.account.render()}
+        <div class="flex">
+          <a class="flex flex-row items-center" href="">
+            <img
+              alt="${this.state.translate('title')}"
+              src="${assets.get('icon.svg')}"
+            />
+          </a>
+        </div>
+        <div class="main-nav flex">
+          <a href="${window.DEFAULTS.LOGIN_URL || ''}">
+            <i class="bx bx-arrow-back"></i>
+            <div>Return to Vault</div>
+          </a>
+          <a href="/vault-logout">
+            <i class="bx bx-exit"></i>
+            <div>Logout</div>
+          </a>
+        </div>
       </header>
     `;
   }
